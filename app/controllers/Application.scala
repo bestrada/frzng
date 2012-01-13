ed095fd92169aafd8d53d8b1cab0c1f3f64520a0
Bryan Estrada <github@bryanestrada.com>
2012-01-13 15:27:50 -0800

    checkpoint

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,12 +4,16 @@ import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
-  
-  def index = Action {
-    Ok(views.html.index())
-  }
+	
+	def index = Action {
+		Ok(views.html.index())
+	}
 
-  def where = Action {
-    Ok(views.html.where())
-  }
+	def where = Action {
+		Ok(views.html.where())
+	}
+	
+	def home(code: String) = Action {
+		Ok(views.html.home(code))
+	}
 }

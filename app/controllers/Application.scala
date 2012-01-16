fd9ecefd5590d9004c3f4e8d10a884f2014eba41
Bryan Estrada <github@bryanestrada.com>
2012-01-16 12:59:48 -0800

    setup Facebook sdk

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,12 +8,17 @@ object Application extends Controller {
 	def index = Action {
 		Ok(views.html.index())
 	}
-
+	
 	def where = Action {
 		Ok(views.html.where())
 	}
 	
 	def home(code: String) = Action {
+		//response.setCookie("foo", "bar", "14d")
 		Ok(views.html.home(code))
+	}
+	
+	def channel = Action {
+		Ok(views.html.channel())
 	}
 }
